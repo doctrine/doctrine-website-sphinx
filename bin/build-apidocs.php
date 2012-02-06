3c0fe29e91195d1d14662129388c9f3e37c3e739
Benjamin Eberlei <kontakt@beberlei.de>
2012-02-06 15:46:40 +0100

    More fixes

--- bin/build-apidocs.php
+++ bin/build-apidocs.php
@@ -7,6 +7,11 @@
 
 require_once __DIR__ . "/../vendor/.composer/autoload.php";
 
+if (!isset($argv[1])) {
+    echo "missing argument\n";
+    exit(1);
+}
+
 $output = $argv[1];
 if (!is_dir($output)) {
     echo "First argument has to be a directory.\n";
@@ -18,11 +23,11 @@ $data = \Symfony\Component\Yaml\Yaml::parse(file_get_contents(__DIR__ . "/../pag
 foreach ($data as $project => $projectDetails) {
     foreach ($projectDetails['versions'] as $version => $versionData) {
 
-        if (isset($versionData['browse_source_link']) && count($versionData['releases'])) {
+        if (isset($versionData['browse_source_link']) && count($versionData['releases']) && isset($versionData['source_path'])) {
             $checkout = end(array_keys($versionData['releases']));
             $url = $versionData['browse_source_link'];
             $path = "source/$project$version";
-            $updateSourceCmd = sprintf("rm -rf %s && git clone %s.git %s && cd %s && git checkout %s", $path, $url, $path, $path, $checkout);
+            $updateSourceCmd = sprintf("rm -rf %s && git clone %s.git %s && cd %s && git checkout %s && git submodule update --init", $path, $url, $path, $path, $checkout);
 
             chdir(__DIR__ . "/../");
             echo "Executing $updateSourceCmd\n";
@@ -33,7 +38,7 @@ foreach ($data as $project => $projectDetails) {
             mkdir($directory);
 
             chdir(__DIR__ . "/../");
-            $apiDocs = sprintf("apigen -s %s -d %s/%s", $versionData['api_source_path'], $output, $versionData['source_path'] );
+            $apiDocs = sprintf('apigen -s %s -d %s/%s --main "%s" --title "%s"', $versionData['api_source_path'], $output, $versionData['source_path'], $projectDetails['slug'], $projectDetails['title'] );
             echo "Generating API Docs: $apiDocs\n";
             shell_exec($apiDocs);
         }
