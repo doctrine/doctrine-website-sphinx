9c6d5f0cc1f7cbf5bd5a533722a1bec6af43f99f
Benjamin Eberlei <kontakt@beberlei.de>
2012-03-06 23:45:31 +0100

    Build also updates api docs now

--- bin/build-apidocs.php
+++ bin/build-apidocs.php
@@ -12,7 +12,7 @@ if (!isset($argv[1])) {
     exit(1);
 }
 
-$output = $argv[1];
+$output = realpath($argv[1]);
 if (!is_dir($output)) {
     echo "First argument has to be a directory.\n";
     exit(1);
@@ -26,19 +26,25 @@ foreach ($data as $project => $projectDetails) {
         if (isset($versionData['browse_source_link']) && count($versionData['releases']) && isset($versionData['source_path'])) {
             $checkout = end(array_keys($versionData['releases']));
             $url = $versionData['browse_source_link'];
-            $path = "source/$project$version";
-            $updateSourceCmd = sprintf("rm -rf %s && git clone %s.git %s && cd %s && git checkout %s && git submodule update --init", $path, $url, $path, $path, $checkout);
+            $path = "source/$project";
+            if (is_dir($path)) {
+                $updateSourceCmd = sprintf("cd %s && git pull && git checkout %s", $path, $checkout);
+            } else {
+                $updateSourceCmd = sprintf("git clone %s.git %s && cd %s && git checkout %s", $url, $path, $path, $checkout);
+            }
 
             chdir(__DIR__ . "/../");
             echo "Executing $updateSourceCmd\n";
             shell_exec($updateSourceCmd);
 
             $directory = $output . "/" . $versionData['source_path'];
-            echo "Creating directory: $directory\n";
-            mkdir($directory);
+            if (!file_exists($directory)) {
+                echo "Creating directory: $directory\n";
+                mkdir($directory);
+            }
 
             chdir(__DIR__ . "/../");
-            $apiDocs = sprintf('apigen -s %s -d %s/%s --main "%s" --title "%s"', $versionData['api_source_path'], $output, $versionData['source_path'], $projectDetails['slug'], $projectDetails['title'] );
+            $apiDocs = sprintf('apigen -s %s -d %s/%s --main "%s" --title "%s"', $path.'/lib/Doctrine', $output, $versionData['source_path'], $projectDetails['slug'], $projectDetails['title'] );
             echo "Generating API Docs: $apiDocs\n";
             shell_exec($apiDocs);
         }
